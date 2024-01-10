2066a07c3ad32e33f011ef11fc99f98e6e17752a
DillonBorden <dillondaughtry2012@gmail.com>
2024-01-09 20:32:13 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+function saturdayFun(sumValue = "roller-skate"){
+    return `This Saturday, I want to ${sumValue}!`
+}
+
+const mondayWork = function(sumValue = "go to the office"){
+    return `This Monday, I will ${sumValue}.`
+
+}
+
+function wrapAdjective(flair = "*"){
+    const innerFunction = function(sumValue = "special"){
+        return `You are ${flair}${sumValue}${flair}!`;
+    };
+    return innerFunction;
+
+}
